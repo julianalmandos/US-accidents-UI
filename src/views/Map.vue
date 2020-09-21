54c3fd8ef04e337a8c6aabd29686c86de118e6de
Leonel Mandarino <leonelmandarino@gmail.com>
2020-09-21 20:21:06 -0300

    Add circle around markers

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="map-container">
     <Panel @update-marks="updateMarks" />
-    <l-map class="map" ref="myMap" :zoom="zoom" :center="center">
+    <l-map class="map" ref="myMap" :zoom="zoom" :center="center" :markerZoomAnimation="true">
       <l-tile-layer :url="url"></l-tile-layer>
       <l-marker v-for="(mark, index) in markers" :key="index" :lat-lng="mark" />
+      <l-circle
+        v-if="circle.center"
+        :lat-lng="circle.center"
+        :radius="circle.radius"
+        :color="circle.color"
+      />
     </l-map>
   </div>
 </template>
 
 <script>
 import L from "leaflet";
-import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { LMap, LTileLayer, LMarker, LCircle } from "vue2-leaflet";
 import Panel from "@/components/Panel.vue";
 
 export default {
@@ -19,6 +25,7 @@ export default {
     LMap,
     LTileLayer,
     LMarker,
+    LCircle,
     Panel,
   },
   data() {
@@ -27,11 +34,26 @@ export default {
       zoom: 3,
       center: L.latLng(47.41322, -1.219482),
       markers: [],
+      circle: {
+        center: null,
+        radius: 0,
+        color: "red",
+      },
     };
   },
   methods: {
-    updateMarks(newMarks) {
-      this.markers = newMarks;
+    updateMarks(data) {
+      let lat = data.parameters.latitude;
+      let lng = data.parameters.longitude;
+      let rad = data.parameters.radius;
+
+      this.markers = data.coords;
+      this.circle.radius = rad * 1000;
+      this.circle.center = [lat, lng];
+      // Vue Leaflet library doesn't work properly when updating these two,
+      // so I change the properties of the map directly https://github.com/vue-leaflet/Vue2Leaflet/issues/170
+      this.$refs.myMap.setZoom(10);
+      this.$refs.myMap.setCenter([lat, lng]);
     },
   },
 };
